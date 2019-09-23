70937f0ab392723da35c43ec48613702b7dbf6b0
Jason Kempel <kempel.jason13@gmail.com>
2019-09-23 11:46:44 -0500

    got game started

--- src/game.java
+++ src/game.java
@@ -1,5 +1,68 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class game {
     public static void main(String[] args){
-        System.out.println("Heloo");
+        Scanner input = new Scanner(System.in);
+
+    startGame(input);
+    enemy1();
+    damageDone();
+    attack();
+
+
+
     }
+    public static void startGame (Scanner input) {
+        System.out.println("Are you ready to start the adventure!");
+        String response = input.nextLine();
+
+        if (response.equalsIgnoreCase("yes")){
+            hero(input);
+        }
+    }
+
+    public static void enemy1 () {
+        System.out.println("\n===================");
+        System.out.println("Your enemy is: Frankenstein");
+        int enemyHealth = 100;
+        System.out.printf("Health: %s", enemyHealth);
+        System.out.println("\nWeakness: Fire");
+    }
+
+    public static void hero (Scanner input) {
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        System.out.printf("Welcome %s", name);
+
+        int health = 100;
+        int damageDone = damageDone();
+        int sheild = 85;
+
+        System.out.printf("\nYour Health: %s", health);
+        System.out.printf("\nYour shield: %s", sheild);
+    }
+
+    public static int damageDone () {
+        int max = 30;
+        int min = 15;
+        int range = max- min +1;
+        int attackPoints = (int) (Math.random() *range) + min;
+        return attackPoints;
+    }
+
+    public static void attack () {
+        int strike = damageDone();
+        int enemyHealth = 100;
+        int healthAfterStrike = enemyHealth - strike;
+        System.out.println("Your hit caused " + strike + "% damage!");
+        System.out.println("Frankenstein's health is now at " + healthAfterStrike + "%");
+    }
+
+
+
+
+
+
+
 }
